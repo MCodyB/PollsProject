1fc90b7eaa6dd95c4ffc7fc056e77f3c4c6bdb59
Codyz+Codyb <Codyz+Codyb@appacademy.io>
2013-05-07 16:00:07 -0400

    Finished poll validations

--- app/models/response.rb
+++ app/models/response.rb
@@ -6,15 +6,25 @@ class Response < ActiveRecord::Base
 	has_one :question, :through => :answer
 
 	validates :user_id, :answer_id, :reply, :presence => true
-	# validate :user_different_from_creator
-	#
-	# def user_different_from_creator
-	# 	self.user_id != self.question.poll.user_id
-	#   end
+	validate :user_different_from_creator
+	validate :reply_in_answers
 
 	def self.create_response(user_id, answer_id, reply)
 		self.create!(user_id: user_id,
 			 					 answer_id: answer_id,
 								 reply: reply)
 	end
+
+	def user_different_from_creator
+		if self.user.id == self.question.poll.user_id
+			errors[:user_id] << "Stop trying to pad your polling numbers."
+		end
+	end
+
+	def reply_in_answers
+		unless self.answer.pluck(:body).include?(reply)
+			errors[:answer_id] << "Stop trying to pad your polling numbers."
+	end
+
+
 end
